aa6e6a2f162a6ec4effe5721cce3bcddfa28a8f6
DESKTOP-0KQ1UDC\tantr <tantrares@yahoo.com>
2022-03-26 19:53:41 +0200

    Update run.js

--- Lab3/Hamming_4biti/run.js
+++ Lab3/Hamming_4biti/run.js
@@ -1,14 +1,14 @@
-var api = require("./api.js ").app;
-var hamming = require("./hamming.js ");
+var api = require("./api.js").app;
+var hamming = require("./hamming.js");
 api.put("/ message ", function (request, response) {
   var bits = request.body.bits;
   // bits = distortBit (bits , 2);
 
   var decoded = hamming.decode(bits);
   if (decoded.errorCorrected) {
-    response.json("One error corrected on position : " + decoded.errorPosition);
+    response.json("One error corrected on position:" + decoded.errorPosition);
   }
-  response.json("Message received without errors ");
+  response.json("Message received without errors");
 });
 
 api.listen(3000, function () {
