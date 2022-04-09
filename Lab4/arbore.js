8f97227eff9c0be9a6750a10b489b569df134c19
DESKTOP-0KQ1UDC\tantr <tantrares@yahoo.com>
2022-04-09 21:29:43 +0300

    commenting the "hardcoded" ex

--- Lab4/arbore.js
+++ Lab4/arbore.js
@@ -6,79 +6,79 @@
 // un "0") si R - dreapta ( introduce un "1") )
 //  Numele nodului contine numele nodului vizitat anterior .
 
-var treeData = {
-  name: "A0",
-  children: [
-    {
-      name: "Level 1: A0R",
-      children: [
-        {
-          name: "Level 2: A0RR",
-          children: [
-            {
-              name: "Level 3: A0RRR",
-              children: [
-                {
-                  name: "Level 4: A0RRRR",
-                  children: [
-                    { name: "Level 5: A0RRRRR = V" },
-                    { name: "Level 5: A0RRRRL = S" },
-                  ],
-                },
-                {
-                  name: "Level 4: A0RRRL",
-                  children: [
-                    { name: "Level 5: A0RRRLR = L" },
-                    { name: "Level 5: A0RRRLL = H" },
-                  ],
-                },
-              ],
-            },
-            {
-              name: "Level 3: A0RRL",
-              children: [
-                {
-                  name: "Level 4: A0RRLR",
-                  children: [
-                    { name: "Level 5: A0RRLRR = B" },
-                    { name: "Level 5: A0RRLRL = ." },
-                  ],
-                },
-                { name: "Level 4: A0RRLL = N" },
-              ],
-            },
-          ],
-        },
-        {
-          name: "Level 2: A0RL",
-          children: [
-            {
-              name: "Level 3: A0RLR",
-              children: [
-                { name: "Level 4: A0RLRR = I" },
-                { name: "Level 4: A0RLRL = A" },
-              ],
-            },
-            { name: "Level 3: A0RLL = R" },
-          ],
-        },
-      ],
-    },
-    {
-      name: "Level 1: A0L",
-      children: [
-        {
-          name: "Level 2: A0LR",
-          children: [
-            { name: "Level 3: A0LRR = T" },
-            { name: "Level 3: A0LRL = E" },
-          ],
-        },
-        { name: "Level 2: A0LL = spatiu" },
-      ],
-    },
-  ],
-};
+// var treeData = {
+//   name: "A0",
+//   children: [
+//     {
+//       name: "Level 1: A0R",
+//       children: [
+//         {
+//           name: "Level 2: A0RR",
+//           children: [
+//             {
+//               name: "Level 3: A0RRR",
+//               children: [
+//                 {
+//                   name: "Level 4: A0RRRR",
+//                   children: [
+//                     { name: "Level 5: A0RRRRR = V" },
+//                     { name: "Level 5: A0RRRRL = S" },
+//                   ],
+//                 },
+//                 {
+//                   name: "Level 4: A0RRRL",
+//                   children: [
+//                     { name: "Level 5: A0RRRLR = L" },
+//                     { name: "Level 5: A0RRRLL = H" },
+//                   ],
+//                 },
+//               ],
+//             },
+//             {
+//               name: "Level 3: A0RRL",
+//               children: [
+//                 {
+//                   name: "Level 4: A0RRLR",
+//                   children: [
+//                     { name: "Level 5: A0RRLRR = B" },
+//                     { name: "Level 5: A0RRLRL = ." },
+//                   ],
+//                 },
+//                 { name: "Level 4: A0RRLL = N" },
+//               ],
+//             },
+//           ],
+//         },
+//         {
+//           name: "Level 2: A0RL",
+//           children: [
+//             {
+//               name: "Level 3: A0RLR",
+//               children: [
+//                 { name: "Level 4: A0RLRR = I" },
+//                 { name: "Level 4: A0RLRL = A" },
+//               ],
+//             },
+//             { name: "Level 3: A0RLL = R" },
+//           ],
+//         },
+//       ],
+//     },
+//     {
+//       name: "Level 1: A0L",
+//       children: [
+//         {
+//           name: "Level 2: A0LR",
+//           children: [
+//             { name: "Level 3: A0LRR = T" },
+//             { name: "Level 3: A0LRL = E" },
+//           ],
+//         },
+//         { name: "Level 2: A0LL = spatiu" },
+//       ],
+//     },
+//   ],
+// };
 
 // %Definirea dimensiunii de afisare a arborelui
 var margin = { top: 20, right: 90, bottom: 30, left: 90 },
